390a2899989c0a28c57e74d4d72f10a15b8e3032
James W. Lane III <james.w.lane@mac.com>
2024-06-28 11:40:48 -0500

    Update toolbox.sh

--- install/toolbox.sh
+++ install/toolbox.sh
@@ -2,9 +2,10 @@ sudo apt install -y libfuse2
 
 cd /tmp
 curl -sLo jetbrains-toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.3.2.31487.tar.gz"
-tar -xf jetbrains-toolbox.tar.gz jetbrains-toolbox
-# install jetbrains-toolbox
-rm jetbrains-toolbox.tar.gz jetbrains-toolbox
+tar -xf jetbrains-toolbox.tar.gz
+[ ! -d $HOME/Applications ] && mkdir $HOME/Applications
+mv ./jetbrains-toolbox-2.3.2.31487/jetbrains-toolbox $HOME/Applications
+rm -rf jetbrains-toolbox.tar.gz jetbrains-toolbox-2.3.2.31487
 cd -
 
 
