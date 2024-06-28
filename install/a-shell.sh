a634d67dd92b07358d9ddac28b6d7394e7196602
James W Lane <james.w.lane@mac.com>
2024-06-28 17:33:10 -0500

    stopped setting shell

--- install/a-shell.sh
+++ install/a-shell.sh
@@ -2,7 +2,7 @@ sudo apt install -y zsh zplug
 
 [ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
 cp ~/.local/share/devex/configs/zshrc ~/.zshrc
-chsh -s /bin/zsh
+# chsh -s /bin/zsh
 source ~/.local/share/devex/defaults/zsh/shell
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
